30076b2251d60db79e855b884f23934c8ac73965
nikhilNathwani <nnathwani36@gmail.com>
2024-06-26 17:50:19 -0400

    stub for query games 8

--- app.js
+++ app.js
@@ -28,36 +28,36 @@ pool.query("SELECT NOW()", (err, res) => {
 });
 
 // Connect to SQLite database
-const db = new sqlite3.Database("./moneyline.db", (err) => {
-	if (err) {
-		console.error("Could not connect to database", err);
-	} else {
-		console.log("Connected to SQLite database");
-	}
-});
+// const db = new sqlite3.Database("./moneyline.db", (err) => {
+// 	if (err) {
+// 		console.error("Could not connect to database", err);
+// 	} else {
+// 		console.log("Connected to SQLite database");
+// 	}
+// });
 
 // Serve static files from public directory (like css/js files)
 app.use(express.static(path.join(__dirname, "public")));
 
 // Define a route to fetch data from the database
-app.get("/api/games", (req, res) => {
-	console.log("app " + getFilterValues(req));
-	res.json({ message: "Nikhil Logging to the console" });
-	db.all(
-		"SELECT seasonStartYear, team, gameNumber FROM games",
-		[],
-		(err, rows) => {
-			if (err) {
-				res.status(500).json({ error: err.message });
-				return;
-			}
-			res.json({
-				message: "success",
-				data: rows,
-			});
-		}
-	);
-});
+// app.get("/api/games", (req, res) => {
+// 	console.log("app " + getFilterValues(req));
+// 	res.json({ message: "Nikhil Logging to the console" });
+// 	db.all(
+// 		"SELECT seasonStartYear, team, gameNumber FROM games",
+// 		[],
+// 		(err, rows) => {
+// 			if (err) {
+// 				res.status(500).json({ error: err.message });
+// 				return;
+// 			}
+// 			res.json({
+// 				message: "success",
+// 				data: rows,
+// 			});
+// 		}
+// 	);
+// });
 
 // Fallback to serve index.html for any other route
 app.get("*", (req, res) => {
@@ -69,10 +69,10 @@ app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
 
-function getFilterValues(req) {
-	const bet = parseInt(req.query.bet, 10);
-	const team = req.query.team;
-	const outcome = req.query.outcome;
-	const seasonStartYear = parseInt(req.query.seasonStart, 10);
-	return { bet, team, outcome, seasonStartYear };
-}
+// function getFilterValues(req) {
+// 	const bet = parseInt(req.query.bet, 10);
+// 	const team = req.query.team;
+// 	const outcome = req.query.outcome;
+// 	const seasonStartYear = parseInt(req.query.seasonStart, 10);
+// 	return { bet, team, outcome, seasonStartYear };
+// }
