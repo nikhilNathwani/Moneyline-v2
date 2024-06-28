8b20033521a01ed3c288b9453756c6ef379889a1
nikhilNathwani <nnathwani36@gmail.com>
2024-06-28 17:10:16 -0400

    stub for result div-making functions 10

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const pool = new Pool({
 	connectionString: process.env.POSTGRES_URL,
 });
 
+//Route to fetch data
 app.get("/api/games", (req, res) => {
 	const { seasonStart, team } = req.query;
 
@@ -34,7 +35,7 @@ app.get("/api/games", (req, res) => {
 			res.status(500).json({ error: err.message });
 			return;
 		}
-		console.log("Success executing query");
+		console.log("Success executing query:", result.rows, "hi");
 		res.json({
 			message: "success",
 			data: result.rows,
@@ -43,40 +44,20 @@ app.get("/api/games", (req, res) => {
 });
 
 // Example query using the pool
-pool.query(
-	"SELECT seasonStartYear, team, gameNumber, outcome, winOdds, loseOdds FROM games",
-	(err, res) => {
-		console.log("in pool");
-		if (err) {
-			console.error("Error executing query:", err);
-		} else {
-			console.log("Data:", res);
-		}
-	}
-);
+// pool.query(
+// 	"SELECT seasonStartYear, team, gameNumber, outcome, winOdds, loseOdds FROM games",
+// 	(err, res) => {
+// 		console.log("in pool");
+// 		if (err) {
+// 			console.error("Error executing query:", err);
+// 		} else {
+// 			console.log("Data:", res);
+// 		}
+// 	}
+// );
 
 // Serve static files from public directory (like css/js files)
 app.use(express.static(path.join(__dirname, "public")));
-
-// Define a route to fetch data from the database
-// app.get("/api/games", (req, res) => {
-// 	console.log("app " + getFilterValues(req));
-// 	res.json({ message: "Nikhil Logging to the console" });
-// 	db.all(
-// 		"SELECT seasonStartYear, team, gameNumber FROM games",
-// 		[],
-// 		(err, rows) => {
-// 			if (err) {
-// 				res.status(500).json({ error: err.message });
-// 				return;
-// 			}
-// 			res.json({
-// 				message: "success",
-// 				data: rows,
-// 			});
-// 		}
-// 	);
-// });
 
 // Fallback to serve index.html for any other route
 app.get("*", (req, res) => {
