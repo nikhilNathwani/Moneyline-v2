ef8038e028ddaad58d7f55cc1dd49750ac2ac036
nikhilNathwani <nnathwani36@gmail.com>
2025-11-20 15:19:26 -0500

    added data-source text at bottom of result container saying Data from oddsportal.com

--- public/js/view/transitions/fade.js
+++ public/js/view/transitions/fade.js
@@ -1,18 +1,24 @@
 function fadeOutResults() {
 	const results = document.querySelectorAll(".result");
+	const dataSource = document.querySelector("#data-source");
 	if (results.length == 0) return;
 	results.forEach((result) => {
 		result.classList.add("disappear");
 	});
+	dataSource.classList.add("disappear");
 	// Use the first result to get the transition duration
 	return getTransitionDuration(results[0]);
 }
 
 function fadeInResults() {
 	const results = document.querySelectorAll(".result");
+	const dataSource = document.querySelector("#data-source");
 	results.forEach((result, index) => {
 		setTimeout(() => {
 			result.classList.remove("disappear");
+			if (index === results.length - 1) {
+				dataSource.classList.remove("disappear");
+			}
 		}, index * 500);
 	});
 }
